8a67f8c94177f7be384b4b26bf5c8e30f74e1cc1
Oksana <oksanamikhalyncuk@gmail.com>
2026-09-21 00:34:56 +0300

    build: configure webpack and dev server

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            templateContent: '<!DOCTYPE html><html><head><meta charset="utf-8"><title>Library</title></head><body><div id="app"></div></body></html>',
+        }),
+    ],
+    devServer: {
+        static: './dist',
+        port: 9000,
+        hot: true,
+    },
+};
